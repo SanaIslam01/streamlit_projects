4cc33d1151430d1422c8edd7acb1a75f90e96a87
SanaIslam01 <157909658+SanaIslam01@users.noreply.github.com>
2024-05-10 16:14:22 +0500

    Update diabetes.py

--- diabetes.py
+++ diabetes.py
@@ -222,10 +222,7 @@
 import streamlit as st 
 import pandas as pd 
 import numpy as np 
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.svm import SVC
-from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+import sklearn
 
 # Function to read the dataset
 def read_dataset():
